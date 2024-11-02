2b610b37b7c380813aad9da4cbccc58790b62824
DeveloperRalsei <rizayildirim126@gmail.com>
2024-11-02 11:03:28 +0300

    oops enough

--- src/ui/cards/UserCard.tsx
+++ src/ui/cards/UserCard.tsx
@@ -1,22 +1,43 @@
-import { Avatar, Card, Grid, Group, Image, Indicator, Stack, Text, Title } from "@mantine/core";
-import { User } from "../../App";
+import {
+  Avatar,
+  Card,
+  Grid,
+  Group,
+  Image,
+  Indicator,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import { User } from "../../types";
 import { openModal } from "@mantine/modals";
 import { UserModalContent } from "./UserModal";
 
-export default function UserCard({ user }: { user: User; }) {
+export default function UserCard({ user }: { user: User }) {
   function openUserModal() {
     openModal({
       title: user.username,
       children: <UserModalContent user={user} />,
-      size: "lg"
+      size: "lg",
     });
   }
 
   return (
-    <Card bg={"dark"} onClick={openUserModal} radius={10} p={10} mah={250} pos={"relative"} style={{ cursor: "pointer" }}>
+    <Card
+      bg={"dark"}
+      onClick={openUserModal}
+      radius={10}
+      p={10}
+      mah={250}
+      pos={"relative"}
+      style={{ cursor: "pointer" }}>
       <Grid>
         <Grid.Col span={3}>
-          <Indicator position="top-start" color={user.is_online ? "lime" : "gray"} size={15} zIndex={0}>
+          <Indicator
+            position="top-start"
+            color={user.is_online ? "lime" : "gray"}
+            size={15}
+            zIndex={0}>
             <Image src={user.avatar_url} alt="avatar-url" />
           </Indicator>
         </Grid.Col>
