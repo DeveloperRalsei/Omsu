bd9a8cf005076222b86c9d764dceec9aa5b7dd83
DeveloperRalsei <fluffycutegoatboi@gmail.com>
2025-04-01 13:43:26 +0300

    before deploy

--- src/pages_/ReviewNews.tsx
+++
@@ -1,6 +0,0 @@
-import { usePage } from "../componenets/context/PageContext";
-
-export default function ReviewNews() {
-  const { newsletterId } = usePage();
-  return <div>{newsletterId}</div>;
-}
